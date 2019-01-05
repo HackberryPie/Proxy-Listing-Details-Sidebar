966c3284288f7bf41fe7da577ee3cfa48c2e4f52
Sujin Lee <slee1016@gmail.com>
2019-01-05 09:44:50 -0500

    add all component routes

--- proxy.js
+++ proxy.js
@@ -3,12 +3,21 @@ const proxy = require('http-proxy-middleware');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/", proxy({
-    target: "http://127.0.0.1:3012/", 
-    changeOrigin: true 
-}));
+app.use("/:id", express.static(path.join(__dirname, "./client")));
+
+app.use("/api/details",
+    proxy({ target: "http://ec2-18-218-63-198.us-east-2.compute.amazonaws.com/" }));
+
+app.use("/api/carousel",
+    proxy({ target: "http://ec2-18-223-116-251.us-east-2.compute.amazonaws.com/" }));
+
+app.use("/api/similarlistings",
+    proxy({ target: "http://ec2-54-174-166-132.compute-1.amazonaws.com/" }));
+
+app.use("/streetBreezy/api/description",
+    proxy({ target: "http://ec2-18-218-251-40.us-east-2.compute.amazonaws.com:3009/"}));
 
 app.listen(3000, () => console.log('Proxy Server connected!'));
